974d191a4f3655a958d805e8c4d1a0b5b708ef24
alexistdk <alexisndelgado@gmail.com>
2021-06-11 11:48:29 -0300

    add files

---
+++ src/seatmap_parser.py
@@ -0,0 +1,14 @@
+import xml.etree.ElementTree as ET
+import argparse
+import json
+from pathlib import Path
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
